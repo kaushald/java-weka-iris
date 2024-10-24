5577a8ff4ecd8427ebe343728be0dfb771cc004f
Kaushal Dalvi <kaushal.dalvi@ukg.com>
2024-10-23 21:41:18 -0400

    initial commit

---
+++ src/test/java/com/kaushaldalvi/weka/WekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaushaldalvi.weka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
